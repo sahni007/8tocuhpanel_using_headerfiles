3ec1a540d4dea0e37e3e7cded0d232cdcf0281aa
sahni007 <42349105+sahni007@users.noreply.github.com>
2018-10-08 12:43:51 +0530

    update include for emrald

--- include.h
+++ include.h
@@ -3,5 +3,5 @@
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 #include"application.h"
-#include"actiontouchpanel.h"
+#include"actionpanel.h"
 #define _XTAL_FREQ 16000000
